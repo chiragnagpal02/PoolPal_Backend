4e236e2910d1d2db8b5b3d07a3b70df7d1c7eaf6
lyw142 <111484155+lyw142@users.noreply.github.com>
2023-03-18 19:08:13 +0800

    Update Database.py
    Include Carpool and Driver

--- src/databases.py
+++ src/databases.py
@@ -4,7 +4,30 @@ from datetime import datetime
 db = SQLAlchemy()
 
 # Create a passenger DB and a Driver DB and a Carpool DB
+class Carpool(db.Model):
+    __tablename__ = 'carpooling'
 
+    CPID = db.Column(db.String(13), primary_key=True, autoincrement=True)
+    PID = db.Column(db.String(64), nullable=False)
+    DID = db.Column(db.Integer, nullable=False)
+    DriverFee = db.Column(db.Float(precision=2), nullable=False)
+    DateTime = db.Column(db.DateTime, default=datetime.now())
+    CPStartLocation = db.Column(db.String(64), nullable=False)
+    CPendLocation = db.Column(db.String(64), nullable=False)
+    Status = db.Column(db.String(64), nullable=False)
+    Price = db.Column(db.Float(precision=2), nullable=False)
+
+class Driver(db.Model):
+    __tablename__ = 'driver'
+
+    DID = db.Column(db.Float(precision=2), nullable=False, autoincrement=True)
+    DName = db.Column(db.String(64), nullable=False)
+    DAge = db.Column(db.Integer, nullable=False)
+    DGender = db.Column(db.String(1), nullable=False)
+    DEmail = db.Column(db.String(64), nullable=False)
+    DVehicleNo = db.Column(db.String(64), nullable=False)
+    DLicenseNo = db.Column(db.String(64), nullable=False)
+    DPhoneNo = db.Column(db.String(8), nullable=False)
 
 # Passenger DB
 
