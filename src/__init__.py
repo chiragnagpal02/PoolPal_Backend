59e1aabc608bfffb201404368e8edcf4af2ae81f
Chirag Nagpal <chiragwork02@gmail.com>
2023-03-20 23:09:50 +0800

    Passenger and Driver CRUD Done

--- src/__init__.py
+++ src/__init__.py
@@ -1,22 +1,27 @@
 from flask import Flask
 from src.trial import tryme
 from src.stripe import payment
-
-# from src.databases import db
+from src.databases import db
 from src.auth import auth
+from src.carpool import carpool
+from src.driver import driver
+from src.passenger import passenger
 import os
 
 def create_app():
     app = Flask(__name__)
 
     app.config.from_mapping(
-        SECRET_KEY='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',
+        SECRET_KEY='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',
         SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI")
     )
 
-    # db.app = app
-    # db.init_app(app)
+    db.app = app
+    db.init_app(app)
 
+    app.register_blueprint(driver)
+    app.register_blueprint(passenger)
+    app.register_blueprint(carpool)
     app.register_blueprint(tryme)
     app.register_blueprint(payment)
     app.register_blueprint(auth)
--- src/databases.py
+++ src/databases.py
@@ -3,37 +3,28 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
-# Create a passenger DB and a Driver DB and a Carpool DB
-class Carpool(db.Model):
-    __tablename__ = 'carpooling'
-
-    CPID = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    PID = db.Column(db.Integer, nullable=False)
-    DID = db.Column(db.Integer, nullable=False)
-    DriverFee = db.Column(db.Float(precision=2), nullable=False)
-    DateTime = db.Column(db.DateTime, default=datetime.now())
-    CPStartLocation = db.Column(db.String(64), nullable=False)
-    CPendLocation = db.Column(db.String(64), nullable=False)
-    Status = db.Column(db.String(64), nullable=False)
-    Price = db.Column(db.Float(precision=2), nullable=False)
-
+# Creating all the individual Tables inside the db Database -> Each MS to have its own Schema 
 class Driver(db.Model):
     __tablename__ = 'driver'
 
-    DID = db.Column(db.db.Integer, primary_key=True, autoincrement=True)
+    DID = db.Column(db.Integer, primary_key=True, autoincrement=True)
     DName = db.Column(db.String(64), nullable=False)
-    DAge = db.Column(db.Integer, nullable=False)
     DGender = db.Column(db.String(1), nullable=False)
     DEmail = db.Column(db.String(64), nullable=False)
     DVehicleNo = db.Column(db.String(64), nullable=False)
     DLicenseNo = db.Column(db.String(64), nullable=False)
-    DPhoneNo = db.Column(db.String(8), nullable=False)
+    DLicenseExpiration = db.Column(db.DateTime, nullable=True)
+    DPhoneNo = db.Column(db.Integer, nullable=False)
+    DCar = db.Column(db.String(100), nullable=False)
+    DCapacity = db.Column(db.Integer, nullable=False)
+
 
 # Passenger DB
 
 class Passengers(db.Model):
     PID = db.Column(db.Integer, primary_key=True, autoincrement=True)
     PName = db.Column(db.String(200), nullable=False)
+    PUserName = db.Column(db.String(200), nullable=False, unique=True)
     PAge = db.Column(db.Integer, nullable=False)
     PGender = db.Column(db.String(1), nullable=False)
     PEmail = db.Column(db.String(200), nullable=False)
@@ -44,20 +35,45 @@ class Passengers(db.Model):
     
     def __repr__(self) -> str:
         return f'Request >>> {self.project}'
+    
+# Carpool DB
+class Carpool(db.Model):
+    __tablename__ = 'carpooling'
+
+    CPID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    DID = db.Column(db.Integer, db.ForeignKey('driver.DID'), nullable=False)
+    DriverFee = db.Column(db.Float(precision=2), nullable=False)
+    DateTime = db.Column(db.DateTime, default=datetime.now())
+    CPStartLocation = db.Column(db.String(64), nullable=False)
+    CPendLocation = db.Column(db.String(64), nullable=False)
+    Status = db.Column(db.String(64), nullable=False)
+    Price = db.Column(db.Float(precision=2), nullable=False)
+    Capacity = db.Column(db.Integer, nullable=False)
+
+
+# This is for seeing the people who are registering for a particular carpool
+class CarPeople(db.Model):
+    __tablename__ = 'carpeople'
+    CPID = db.Column(db.Integer, db.ForeignKey('carpooling.CPID'), nullable=False)
+    DID = db.Column(db.Integer, db.ForeignKey('driver.DID'), nullable=False)
+    PID = db.Column(db.Integer, db.ForeignKey('passengers.PID'), nullable=False)
+
+    __table_args__ = (
+        db.PrimaryKeyConstraint('CPID', 'PID', ),
+    )
 
 # Review DB
-
 class Review(db.Model):
     __tablename__ = 'review'
 
-    CPID = db.Column(db.Integer, nullable=False)
-    PID = db.Column(db.Integer, nullable=False)
-    DID = db.Column(db.Integer, nullable=False)
+    CPID = db.Column(db.Integer, db.ForeignKey('carpooling.CPID'), nullable=False)
+    PID = db.Column(db.Integer, db.ForeignKey('passengers.PID'), nullable=False)
+    DID = db.Column(db.Integer, db.ForeignKey('driver.DID'), nullable=False)
     DateTime = db.Column(db.DateTime, default=datetime.now())
     RRating = db.Column(db.Integer, nullable=False)
     RDesc = db.Column(db.String(64), nullable=False)
-    Status = db.Column(db.String(64), nullable=False)
-    Price = db.Column(db.Float(precision=2), nullable=False)
+    # Status = db.Column(db.String(64), nullable=False)
+    # Price = db.Column(db.Float(precision=2), nullable=False)
 
     __table_args__ = (
         db.PrimaryKeyConstraint('CPID', 'PID', 'DID'),
@@ -69,20 +85,12 @@ class Dispute(db.Model):
     __tablename__ = 'dispute'
 
     DPID = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    CPID = db.Column(db.Integer, nullable=False)
-    PID = db.Column(db.Integer, nullable=False)
-    DID = db.Column(db.Integer, nullable=False)
+    CPID = db.Column(db.Integer, db.ForeignKey('carpooling.CPID'), nullable=False)
+    PID = db.Column(db.Integer, db.ForeignKey('passengers.PID'), nullable=False)
+    DID = db.Column(db.Integer, db.ForeignKey('driver.DID'), nullable=False)
     SID = db.Column(db.Integer, nullable=False)
     DPDescript = db.Column(db.String(64), nullable=False)
     DateTime = db.Column(db.DateTime, default=datetime.now())
-
-# Staff DB
-
-class Staff(db.Model):
-    __tablename__ = 'staff'
-
-    SID = db.Column(db.Integer, nullable=False)
-    SName = db.Column(db.String(64), nullable=False)
 
 # Payment Log DB
 
@@ -90,19 +98,26 @@ class PaymentLog(db.Model):
     __tablename__ = 'PaymentLog'
 
     PmID = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    CID = db.Column(db.Integer, nullable=False)
-    PID = db.Column(db.Integer, nullable=False)
+    CPID = db.Column(db.Integer, db.ForeignKey('carpooling.CPID'), nullable=False)
+    PID = db.Column(db.Integer, db.ForeignKey('passengers.PID'), nullable=False)
     CPrice = db.Column(db.Float(precision=2), nullable=False)
     Status = db.Column(db.String(64), nullable=False)
     DateTime = db.Column(db.DateTime, default=datetime.now())
 
-# Pricing DB
+# Staff DB
+class Staff(db.Model):
+    __tablename__ = 'staff'
 
+    SID = db.Column(db.Integer, nullable=False, primary_key=True)
+    SName = db.Column(db.String(64), nullable=False)
+
+# Pricing DB
 class Pricing(db.Model):
     __tablename__ = 'pricing'
 
     CPID = db.Column(db.Integer, nullable=False)
     DateTime = db.Column(db.DateTime, default=datetime.now())
+    OriginalPrice = db.Column(db.Float(precision=2), nullable=False)
     CPrice = db.Column(db.Float(precision=2), nullable=False)
 
     __table_args__ = (
